f262c4fe228dffad2e15c71d36c74f9f4620c6d4
Marcos Harbs <marcos.harbs@gmail.com>
2018-07-11 18:59:50 -0300

    Visualização de localizações em modo mapa.

---
+++ OnTheMap/LocationsMapVC.swift
@@ -0,0 +1,14 @@
+//
+//  LocationsMVC.swift
+//  OnTheMap
+//
+//  Created by Marcos Harbs on 11/07/18.
+//  Copyright © 2018 Marcos Harbs. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class LocationsMVC: UIViewController, MKMapViewDelegate {
+    
+}
